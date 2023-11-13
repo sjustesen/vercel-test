3df9bec46916dcf84af16d2a5a0b9ef84d96995b
Simon Justesen <simon.justesen@gmail.com>
2023-11-14 00:04:58 +0100

    Update

--- index.py
+++ index.py
@@ -1,13 +1,5 @@
 import os
-from dotenv import load_dotenv
 from flask import Flask
-from convex import ConvexClient
-
-load_dotenv(".env.local")
-load_dotenv()
-
-client = ConvexClient(os.getenv("CONVEX_URL"))
-
 
 app = Flask(__name__)
 
